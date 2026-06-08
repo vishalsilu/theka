11e5f1d7632843e92fe87f4f93e4a8c0db8b036c
vishalsilu <vishalsaini20027@gmail.com>
2026-06-08 15:42:33 +0530

    removed loacladdress

--- config/email.js
+++ config/email.js
@@ -8,7 +8,6 @@ const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
     secure: true, 
-    localAddress: '0.0.0.0',
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
