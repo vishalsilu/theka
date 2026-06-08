2e79ec959c371878e046e83bd66ad12dc4323f97
vishalsilu <vishalsaini20027@gmail.com>
2026-06-08 16:00:46 +0530

    Some more changes in email config

--- config/email.js
+++ config/email.js
@@ -3,20 +3,20 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+import dns from 'dns'; // Import Node's native DNS module
+
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 587,            
-    secure: false,        
+    port: 587,
+    secure: false, 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
-    // This tells the underlying connection socket to only use IPv4 lookup rules
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000,
-    dns: {
-        family: 4 // Forces IPv4 resolution
+    // FORCE NODE TO ONLY LOOK UP IPV4 ADDRESSES
+    lookup: (hostname, options, callback) => {
+        options.family = 4; // Hard overrides the lookup to IPv4 only
+        return dns.lookup(hostname, options, callback);
     },
     tls: {
         rejectUnauthorized: false 
